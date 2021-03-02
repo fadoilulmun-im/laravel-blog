09c5ecec09495788c80f9be8a8858b6b871c46e0
il <fadoilulmun.im2807@gmail.com.com>
2021-03-02 13:20:15 +0700

    bug fixing

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,84 +1,87 @@
 @extends('template-blog.content')
-@section('section-1')
-	<!-- SECTION 1 -->
-	<div class="section">
-		<!-- container -->
-		<div class="container">
-			<!-- row -->
-            {{-- {{ $popular }} --}}
-			<div id="hot-post" class="row hot-post">
-				<div class="col-md-8 hot-post-left">
-					<!-- post -->
-					<div class="post post-thumb">
-						<a class="post-img" href="{{ route('blog.isi', $popular[0]->slug) }}"><img src="{{ $popular[0]->gambar }}" alt=""></a>
-						<div class="post-body">
-							<div class="post-category">
-								<a href="{{ route('blog.category', $popular[0]->category->slug) }}">{{ $popular[0]->category->name }}</a>
-							</div>
-							<h3 class="post-title title-lg"><a href="{{ route('blog.isi', $popular[0]->slug) }}">{{ $popular[0]->judul }}</a></h3>
-							<div class="post-category">
-                                @foreach ($popular[0]->tags as $item)
-                                    <a href="category.html"  style="font-size: 10px">{{ $item->name }}</a>
-                                @endforeach
+@if (count($data) >= 3)
+    @section('section-1')
+        <!-- SECTION 1 -->
+        <div class="section">
+            <!-- container -->
+            <div class="container">
+                <!-- row -->
+                {{-- {{ $popular }} --}}
+                <div id="hot-post" class="row hot-post">
+                    <div class="col-md-8 hot-post-left">
+                        <!-- post -->
+                        <div class="post post-thumb">
+                            <a class="post-img" href="{{ route('blog.isi', $popular[0]->slug) }}"><img src="{{ $popular[0]->gambar }}" alt=""></a>
+                            <div class="post-body">
+                                <div class="post-category">
+                                    <a href="{{ route('blog.category', $popular[0]->category->slug) }}">{{ $popular[0]->category->name }}</a>
+                                </div>
+                                <h3 class="post-title title-lg"><a href="{{ route('blog.isi', $popular[0]->slug) }}">{{ $popular[0]->judul }}</a></h3>
+                                <div class="post-category">
+                                    @foreach ($popular[0]->tags as $item)
+                                        <a href="category.html"  style="font-size: 10px">{{ $item->name }}</a>
+                                    @endforeach
+                                </div>
+                                <ul class="post-meta">
+                                    <li><a>{{ $popular[0]->users->name }}</a></li>
+                                    <li>{{ $popular[0]->created_at->diffForHumans() }}</li>
+                                </ul>
                             </div>
-                            <ul class="post-meta">
-								<li><a>{{ $popular[0]->users->name }}</a></li>
-								<li>{{ $popular[0]->created_at->diffForHumans() }}</li>
-							</ul>
-						</div>
-					</div>
-					<!-- /post -->
-				</div>
-				<div class="col-md-4 hot-post-right">
-					<!-- post -->
-					<div class="post post-thumb">
-						<a class="post-img" href="{{ route('blog.isi', $popular[1]->slug) }}"><img src="{{ $popular[1]->gambar }}" alt=""></a>
-						<div class="post-body">
-							<div class="post-category">
-								<a href="{{ route('blog.category', $popular[1]->category->slug) }}">{{ $popular[1]->category->name }}</a>
-							</div>
-							<h3 class="post-title"><a href="{{ route('blog.isi', $popular[1]->slug) }}">{{ $popular[1]->judul }}</a></h3>
-							<div class="post-category">
-                                @foreach ($popular[1]->tags as $item)
-                                    <a href="category.html"  style="font-size: 10px">{{ $item->name }}</a>
-                                @endforeach
+                        </div>
+                        <!-- /post -->
+                    </div>
+                    <div class="col-md-4 hot-post-right">
+                        <!-- post -->
+                        <div class="post post-thumb">
+                            <a class="post-img" href="{{ route('blog.isi', $popular[1]->slug) }}"><img src="{{ $popular[1]->gambar }}" alt=""></a>
+                            <div class="post-body">
+                                <div class="post-category">
+                                    <a href="{{ route('blog.category', $popular[1]->category->slug) }}">{{ $popular[1]->category->name }}</a>
+                                </div>
+                                <h3 class="post-title"><a href="{{ route('blog.isi', $popular[1]->slug) }}">{{ $popular[1]->judul }}</a></h3>
+                                <div class="post-category">
+                                    @foreach ($popular[1]->tags as $item)
+                                        <a href="category.html"  style="font-size: 10px">{{ $item->name }}</a>
+                                    @endforeach
+                                </div>
+                                <ul class="post-meta">
+                                    <li><a>{{ $popular[1]->users->name }}</a></li>
+                                    <li>{{ $popular[1]->created_at->diffForHumans() }}</li>
+                                </ul>
                             </div>
-                            <ul class="post-meta">
-								<li><a>{{ $popular[1]->users->name }}</a></li>
-								<li>{{ $popular[1]->created_at->diffForHumans() }}</li>
-							</ul>
-						</div>
-					</div>
-					<!-- /post -->
+                        </div>
+                        <!-- /post -->
 
-					<!-- post -->
-					<div class="post post-thumb">
-						<a class="post-img" href="{{ route('blog.isi', $popular[2]->slug) }}"><img src="{{ $popular[2]->gambar }}" alt=""></a>
-						<div class="post-body">
-							<div class="post-category">
-								<a href="{{ route('blog.category', $popular[2]->category->slug) }}">{{ $popular[2]->category->name }}</a>
-							</div>
-							<h3 class="post-title"><a href="{{ route('blog.isi', $popular[2]->slug) }}">{{ $popular[2]->judul }}</a></h3>
-							<div class="post-category">
-                                @foreach ($popular[2]->tags as $item)
-                                    <a href="{{ route('blog.tag', $item->slug) }}"  style="font-size: 10px">{{ $item->name }}</a>
-                                @endforeach
+                        <!-- post -->
+                        <div class="post post-thumb">
+                            <a class="post-img" href="{{ route('blog.isi', $popular[2]->slug) }}"><img src="{{ $popular[2]->gambar }}" alt=""></a>
+                            <div class="post-body">
+                                <div class="post-category">
+                                    <a href="{{ route('blog.category', $popular[2]->category->slug) }}">{{ $popular[2]->category->name }}</a>
+                                </div>
+                                <h3 class="post-title"><a href="{{ route('blog.isi', $popular[2]->slug) }}">{{ $popular[2]->judul }}</a></h3>
+                                <div class="post-category">
+                                    @foreach ($popular[2]->tags as $item)
+                                        <a href="{{ route('blog.tag', $item->slug) }}"  style="font-size: 10px">{{ $item->name }}</a>
+                                    @endforeach
+                                </div>
+                                <ul class="post-meta">
+                                    <li><a>{{ $popular[2]->users->name }}</a></li>
+                                    <li>{{ $popular[2]->created_at->diffForHumans() }}</li>
+                                </ul>
                             </div>
-                            <ul class="post-meta">
-								<li><a>{{ $popular[2]->users->name }}</a></li>
-								<li>{{ $popular[2]->created_at->diffForHumans() }}</li>
-							</ul>
-						</div>
-					</div>
-					<!-- /post -->
-				</div>
-			</div>
-			<!-- /row -->
-		</div>
-		<!-- /container -->
-	</div>
-	<!-- /SECTION -->
-@endsection
+                        </div>
+                        <!-- /post -->
+                    </div>
+                </div>
+                <!-- /row -->
+            </div>
+            <!-- /container -->
+        </div>
+        <!-- /SECTION -->
+    @endsection
+@endif
+
 
 @section('section-2')
 	<!-- row -->
